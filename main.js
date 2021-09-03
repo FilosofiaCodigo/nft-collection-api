aa4155d0db8d7a6ce445c95337b6379c242403cf
turupawn <ahmed.hn.43@gmail.com>
2021-09-03 00:08:55 -0600

    passing json

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
 app.use(express.static(__dirname + 'public'))
 app.use('/images', express.static(__dirname + '/images'));
 
